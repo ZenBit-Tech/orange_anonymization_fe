d085906795b9baae39b1b97689df71379e9a7d99
Oleksandr Cheban <cebanoleksandr@gmail.com>
2026-03-30 15:59:26 +0300

    Create 2 pages as example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,10 @@ import { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { CircularProgress, Box } from '@mui/material';
 import { ROUTES } from '@/constants';
+import Auth from '@/pages/Auth';
+import { ProtectedRoute } from './ProtectedRoute';
+import { MainLayout } from '@/components/layouts/MainLayout';
+import Dashboard from '@/pages/Dashboard';
 
 function PageLoader() {
   return (
@@ -15,6 +19,13 @@ export function AppRoutes() {
   return (
     <Suspense fallback={<PageLoader />}>
       <Routes>
+        <Route path="/auth/*" element={<Auth />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+          </Route>
+        </Route>
         <Route path="*" element={<Navigate to={ROUTES.LANDING} replace />} />
       </Routes>
     </Suspense>
