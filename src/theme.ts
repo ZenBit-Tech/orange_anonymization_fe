edcc17fea931ad6dd9f41e511141dc54a9f22a08
Oleksandr Cheban <cebanoleksandr@gmail.com>
2026-04-08 13:27:44 +0300

    Small fix

--- src/theme.ts
+++ src/theme.ts
@@ -102,6 +102,7 @@ declare module '@mui/material/styles' {
     sidebar?: SidebarPalette;
   }
   interface PaletteColor {
+    300?: string;
     400?: string;
     500?: string;
     700?: string;
@@ -110,6 +111,7 @@ declare module '@mui/material/styles' {
     hover?: string;
   }
   interface SimplePaletteColorOptions {
+    300?: string;
     400?: string;
     500?: string;
     700?: string;
