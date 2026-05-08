86a398c505caf17879c4de232742c31a938e79a1
Марк Земледух <146758649+MarkTheBoy@users.noreply.github.com>
2026-05-08 17:51:29 +0300

    test(contact): add form, interceptor, and 404 specs (#34)
    * test(contact): add form, interceptor, and 404 specs
    
    * moved tests to 'test' folder

--- src/test/renderWithProviders.tsx
+++ src/test/renderWithProviders.tsx
@@ -1,7 +1,13 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
 import { store } from '@/store/store';
+import { theme } from '@/theme';
 
 export function renderWithProviders(ui: React.ReactElement) {
-  return render(<Provider store={store}>{ui}</Provider>);
+  return render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{ui}</ThemeProvider>
+    </Provider>,
+  );
 }
